8a04c56989f8b9dd36a3095dfed3c26bb40a426e
ittew <873077939@qq.com>
2020-08-12 22:09:06 +0800

    feat:初始化项目

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>初始化项目</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
