13419894bd7e144fb03497180074901f0dfae135
ittew <873077939@qq.com>
2020-08-13 22:33:34 +0800

    fix: 创建页面骨架

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Home from './components/Home.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
-    history,
-    routes: [
-        {path: '/', component: Home}
-    ]
+  history,
+  routes: [
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc},
+  ]
 })
 const app = createApp(App)
 app.use(router)
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <ul class="menu">
+        <li>菜单一</li>
+        <li>菜单二</li>
+      </ul>
+    </div>
+    <div class="banner">
+      <h1>lea-ui</h1>
+      <h2>练习的UI框架</h2>
+      <p class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+export default {
+
+}
+</script>
+<style lang="scss" scoped>
+.topnav {
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
+</style>
