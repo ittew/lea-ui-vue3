feaf5a5d33f865e86ff49015c31c2140a300c0c4
ittew <873077939@qq.com>
2020-08-14 11:23:55 +0800

    fix: 基本完成整体框架

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,18 @@
 
 <script lang="ts">
 import { ref, provide } from 'vue'
+import { router } from "./router";
 export default {
   name: 'App',
   setup () {
     const clientWidth = document.documentElement.clientWidth
     const menuVisible = ref(clientWidth<=500?false:true)
     provide('menu', menuVisible)
+    router.afterEach(() => {
+      if (clientWidth <= 500) {
+        menuVisible.value = false;
+      }
+    })
   }
 }
 </script>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p v-for="n in 100" :key="n">Dialog 文档</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
