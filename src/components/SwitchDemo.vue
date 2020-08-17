bfb332b6929c2eabedaf94aa64d42c8e34269688
ittew <873077939@qq.com>
2020-08-17 10:48:25 +0800

    feat: 增加switch组件

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Switch v-model:value="bool" />
+  </div>
+</template>
+
+<script>
+import Switch from '../lib/Switch.vue'
+import { ref } from 'vue';
+export default {
+  components: {
+    Switch
+  },
+  setup(){
+    const bool = ref(false)
+    return { bool }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,54 @@
+<template>
+  <button :class="{'checked':value}" @click="toggle"><span></span></button>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    value: Boolean
+  },
+  setup(props, context){
+    const toggle = () => {
+      context.emit('update:value', !props.value)
+    }
+    return {toggle}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button{
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
+  outline: none;
+  transition: all 250ms;
+  span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background:white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.checked{
+    background: #1890ff;
+    span{
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+  &:active{
+    span {width: $h2 + 4px;}
+  }
+  &.checked:active{
+    span {width: $h2 + 4px; margin-left: -4px;}
+  }
+}
+</style>
